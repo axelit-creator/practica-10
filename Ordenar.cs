7d81479a82b193d5790755d030b1ed1a60834770
CarlossDKitz <almn.carlosquitzaman@cbtis66.edu.mx>
2024-06-20 15:48:48 -0600

    prueba

--- Ordenar.cs
+++ Ordenar.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace practica_10
+namespace practica_10//llllllllllll
 {
     public partial class Ordenar : Form
     {
